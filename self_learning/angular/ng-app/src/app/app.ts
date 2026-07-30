67ae2723553dd998bdbc3f6b9661a65bc25350c8
¨Laszlo <laszlo.b.ludescher@gmail.com>
2026-07-30 09:06:38 +0300

    making app.html  responsive

--- self_learning/angular/ng-app/src/app/app.ts
+++ self_learning/angular/ng-app/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButton],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('ng-app');
