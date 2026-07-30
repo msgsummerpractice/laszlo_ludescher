450dfd0839d20c17db15e45f74ab2b98ac0f48cb
¨Laszlo <laszlo.b.ludescher@gmail.com>
2026-07-30 10:38:51 +0300

    add top navbar with angular material

--- self_learning/angular/ng-app/src/app/app.ts
+++ self_learning/angular/ng-app/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButton],
+  imports: [RouterOutlet, MatButton, MatToolbarRow, MatIcon],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
