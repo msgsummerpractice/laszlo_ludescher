4bd113022cee2e0b3083a8fc79e6dcc08fc177fd
¨Laszlo <laszlo.b.ludescher@gmail.com>
2026-08-07 13:02:52 +0300

    change env variable in frontend

--- self_learning/angular/ng-app/src/environments/environment.development.ts
+++ self_learning/angular/ng-app/src/environments/environment.development.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  apiUrl:
-    'https://laszlo-container-backend.internal.ashywater-8898f957.westeurope.azurecontainerapps.io',
-  //apiUrl: 'http://localhost:8081',
+  apiUrl: 'http://localhost:8081',
 };
